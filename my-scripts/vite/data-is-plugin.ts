716f5eafd48cd1435dd1162567c357d57f2268fb
Masato Ohira <ohira.ekzm@gmail.com>
2023-09-10 07:52:49 +0900

    fix

--- my-scripts/vite/data-is-plugin.ts
+++ my-scripts/vite/data-is-plugin.ts
@@ -21,14 +21,12 @@ export const dataIsPlugin = () => {
           // インポートしたtsxコンポーネントのタグにdata-testid属性を追加する
           const componentId = split(id, '/').reverse()[0].replace('.tsx', '')
           const flag = `return /* @__PURE__ */ _jsx`
-          const blankTag = `return /* @__PURE__ */ _jsx(_Fragment`
-
           let codes = split(code, '\n')
 
-          if (code.includes(flag) && !code.includes(blankTag)) {
+          if (code.includes(flag)) {
             let count = 0
             map(codes, (i, key) => {
-              if (i.includes(flag)) {
+              if (i.includes(flag) && !i.includes('_Fragment')) {
                 count++
                 const dataId =
                   count > 1 ? `${componentId}_${count}` : componentId
